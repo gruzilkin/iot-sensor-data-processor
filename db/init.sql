c06e973d677fd75e0443a66390bf8ef35468ac40
Sergey Suglobov <gruzmob@gmail.com>
2021-08-25 13:31:20 +0900

    adds device_id to tables

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,2 @@
-CREATE TABLE temperature(id serial, value numeric, received_at timestamp);
-CREATE TABLE humidity(id serial, value numeric, received_at timestamp);
+CREATE TABLE temperature(id serial, device_id varchar(32), value numeric, received_at timestamp);
+CREATE TABLE humidity(id serial, device_id varchar(32), value numeric, received_at timestamp);
