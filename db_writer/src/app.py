b5c4d63408188eca9048b0154281be1eaac30b31
Sergey Suglobov <gruzmob@gmail.com>
2021-08-25 22:00:57 +0900

    adds db connection to python worker

--- db_writer/src/app.py
+++ db_writer/src/app.py
@@ -1,5 +1,47 @@
-import pika, sys, os
+import psycopg2, pika, json, datetime, sys, os
 from retry import retry
+from functools import partial
+
+def temperature_callback(ch, method, properties, body, connection):
+    timestamp = properties.headers['timestamp_in_ms']
+    body_dict = json.loads(body)
+    
+    received_at = datetime.datetime.fromtimestamp(timestamp/1000)
+    device = body_dict['device']
+    temperature = body_dict['temperature']
+
+    args = (device, temperature, received_at)
+
+    cur = connection.cursor()
+    cur.execute("INSERT INTO temperature (device_id, value, received_at) VALUES (%s, %s, %s)", args)
+    connection.commit()
+    cur.close()
+
+    print(" [x] Received %r" % body)
+
+def humidity_callback(ch, method, properties, body, connection):
+    timestamp = properties.headers['timestamp_in_ms']
+    body_dict = json.loads(body)
+    
+    received_at = datetime.datetime.fromtimestamp(timestamp/1000)
+    device = body_dict['device']
+    humidity = body_dict['humidity']
+
+    args = (device, humidity, received_at)
+
+    cur = connection.cursor()
+    cur.execute("INSERT INTO humidity (device_id, value, received_at) VALUES (%s, %s, %s)", args)
+    connection.commit()
+    cur.close()
+
+    print(" [x] Received %r" % body)
+
+def create_connection():
+    dbname = os.environ['DB_NAME']
+    user = os.environ['DB_USER']
+    password = os.environ['DB_PASSWORD']
+    host = os.environ['DB_HOST']
+    return psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 
 @retry(pika.exceptions.AMQPConnectionError, delay=10, tries=30)
 def main():
@@ -16,11 +58,9 @@ def main():
     channel.queue_declare(queue='Humidity', durable=True)
     channel.queue_bind(queue='Humidity', exchange="amq.topic")
 
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
-
-    channel.basic_consume( queue='Temperature', on_message_callback=callback, auto_ack=True)
-    channel.basic_consume(queue='Humidity', on_message_callback=callback, auto_ack=True)
+    conn = create_connection()
+    channel.basic_consume( queue='Temperature', on_message_callback=partial(temperature_callback, connection=conn), auto_ack=True)
+    channel.basic_consume(queue='Humidity', on_message_callback=partial(humidity_callback, connection=conn), auto_ack=True)
 
     channel.start_consuming()
     
