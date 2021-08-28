4b4e46bd0cf5c90d67ef3af602176b55ce9c0f08
Sergey Suglobov <gruzmob@gmail.com>
2021-08-29 00:20:16 +0900

    disables https redirection

--- web/Startup.cs
+++ web/Startup.cs
@@ -38,8 +38,6 @@ namespace web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
