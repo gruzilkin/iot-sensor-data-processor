9e8e67b2bfbe1b2a4d8f1271fad6656e5d4c5757
Sergey Suglobov <gruzmob@gmail.com>
2021-08-31 22:28:10 +0900

    adds calibration model creation

--- db/1-tables.sql
+++ db/1-tables.sql
@@ -9,3 +9,5 @@ CREATE TABLE calibration (
     ppm numeric NOT NULL,
     received_at timestamp NOT NULL
 );
+
+CREATE TABLE calibration_models (device_id varchar(32) PRIMARY KEY, model bytea NOT NULL, created_at timestamp NOT NULL);
--- db_writer/src/app.py
+++ db_writer/src/app.py
@@ -2,6 +2,85 @@ import psycopg2, pika, json, datetime, sys, os
 from retry import retry
 from functools import partial
 
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+import pickle
+
+def buildCalibrationModel(connection, device_id):
+    cur = connection.cursor()
+    read_calibration_data_command = "SELECT temperature, humidity, r0 " \
+    "FROM calibration " \
+    "WHERE device_id = %s "
+    cur.execute(read_calibration_data_command, (device_id,))
+    calibration_data = cur.fetchall()
+    calibration_data = np.array(calibration_data, dtype=float)
+    train_x = calibration_data[:, :2]
+    train_y = calibration_data[:, -1].reshape(-1, 1)
+
+    model = make_pipeline(StandardScaler(), PolynomialFeatures(degree = 2), LinearRegression())
+    model.fit(train_x, train_y)
+
+    return model
+
+def calibrationModel(connection, device_id):
+    cur = connection.cursor()
+    
+    sql_command = "SELECT cm.model " \
+    "FROM calibration_models cm " \
+    "WHERE cm.device_id = %s AND cm.created_at > ( " \
+	"SELECT MAX(c.received_at) " \
+	"FROM calibration c " \
+	"WHERE c.device_id = %s)"
+
+    cur.execute(sql_command, (device_id, device_id))
+    row = cur.fetchone()
+    cur.close()
+
+    if row is not None:
+        return pickle.loads(row[0])
+    else:
+        model = buildCalibrationModel(connection, device_id)
+        serialized_model = pickle.dumps(model)
+        save_model_sql_command = "INSERT INTO calibration_models (device_id, model, created_at)" \
+                                "VALUES (%s, %s, now()) " \
+                                "ON CONFLICT(device_id) DO UPDATE SET model=%s, created_at=now();"
+        cur.execute(save_model_sql_command, (device_id, serialized_model, serialized_model))
+        connection.commit()
+        cur.close()
+        return model        
+
+
+def calculateR0(connection, device_id, temperature, humidity):
+    pipe = calibrationModel(connection, device_id)
+
+    input = np.array([temperature, humidity], dtype=float).reshape(1, -1)
+    predicted_r0 = pipe.predict(input).item()
+
+    return predicted_r0
+
+
+def calibration_request_callback(ch, method, properties, body, args):
+    (connection, ) = args
+    
+    print(" [x] Received %r" % body)
+
+    device_id = method.routing_key.split('.')[-1]
+    body_dict = json.loads(body)
+    temperature = body_dict['temperature']
+    humidity = body_dict['humidity']
+
+    r0 = calculateR0(connection, device_id, temperature, humidity)
+    response = json.dumps({'r0': r0})
+
+    ch.basic_publish(exchange='amq.topic',
+                     routing_key=f"calibration.response.{device_id}",
+                     body=response)
+
+
 def rabbit_callback(ch, method, properties, body, args):
     (connection, table_name, body_dict_keys) = args
     
@@ -45,13 +124,18 @@ def main():
     channel.queue_declare(queue='calibration', durable=True)
     channel.queue_bind(routing_key="calibration.*", queue='calibration', exchange="amq.topic")
 
+    channel.queue_declare(queue='calibration.request', durable=True)
+    channel.queue_bind(routing_key="calibration.request.*", queue='calibration.request', exchange="amq.topic")
+
     conn = create_connection()
     temperature_callback = partial(rabbit_callback, args=(conn, 'temperature', ['temperature']))
     humidity_callback = partial(rabbit_callback, args=(conn, 'humidity', ['humidity']))
     calibration_callback = partial(rabbit_callback, args=(conn, 'calibration', ['temperature', 'humidity', 'r0', 'ppm']))
+    partial_calibration_request_callback = partial(calibration_request_callback, args=(conn,))
     channel.basic_consume(queue='temperature', on_message_callback=temperature_callback, auto_ack=True)
     channel.basic_consume(queue='humidity', on_message_callback=humidity_callback, auto_ack=True)
     channel.basic_consume(queue='calibration', on_message_callback=calibration_callback, auto_ack=True)
+    channel.basic_consume(queue='calibration.request', on_message_callback=partial_calibration_request_callback, auto_ack=True)
 
     try:
         channel.start_consuming()
