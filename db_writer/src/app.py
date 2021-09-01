324eeeea9e1fbc0e50496356a81b36ebb3d44f93
Sergey Suglobov <gruzmob@gmail.com>
2021-09-01 21:35:15 +0900

    it might be better to just limit sample size

--- db_writer/src/app.py
+++ db_writer/src/app.py
@@ -14,16 +14,18 @@ import pickle
 
 def buildCalibrationModel(connection, device_id):
     with connection.cursor() as cur:
-        read_calibration_data_command = "SELECT round(temperature, 1), round(humidity), round(r0) " \
+        
+        read_calibration_data_command = "SELECT round(temperature, 1) as t, round(humidity) as h, AVG(r0) as r0 " \
         "FROM sensor_calibration_data " \
-        "WHERE device_id = %s "
+        "WHERE device_id = %s " \
+        "GROUP BY device_id, t, h "
         cur.execute(read_calibration_data_command, (device_id,))
         calibration_data = cur.fetchall()
         calibration_data = np.array(calibration_data, dtype=float)
         train_x = calibration_data[:, :2]
         train_y = calibration_data[:, -1].reshape(-1, 1)
 
-        model = make_pipeline(StandardScaler(), PolynomialFeatures(degree = 3), LinearRegression())
+        model = make_pipeline(StandardScaler(), PolynomialFeatures(degree = 2), LinearRegression())
         model.fit(train_x, train_y)
 
         return model
