c6f65eed80ef056637696a652da2549e0e32c4b8
Sergey Suglobov <gruzmob@gmail.com>
2021-09-04 23:37:54 +0900

    automatic outlier detection draft

--- db/1-tables.sql
+++ db/1-tables.sql
@@ -15,7 +15,8 @@ CREATE TABLE sensor_calibration_data (
     r0 numeric NOT NULL,
     ppm numeric NOT NULL,
     uptime interval NOT NULL,
-    received_at timestamp NOT NULL
+    received_at timestamp NOT NULL,
+    is_outlier boolean DEFAULT false
 );
 
 CREATE TABLE calibration_models (device_id varchar(32) PRIMARY KEY, model bytea NOT NULL, created_at timestamp NOT NULL);
