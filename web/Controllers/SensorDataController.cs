18a27f9b9eac544e64588078634150fe363be725
Sergey Suglobov <gruzmob@gmail.com>
2021-09-08 23:41:56 +0900

    makes webservice return history from db before returning data from queue

--- web/Controllers/SensorDataController.cs
+++ web/Controllers/SensorDataController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using web.Db;
 using web.Services;
 
 namespace web.Controllers
@@ -19,10 +21,13 @@ namespace web.Controllers
 
         private readonly RabbitMQService rabbit;
 
-        public SensorDataController(ILogger<SensorDataController> logger, RabbitMQService rabbit)
+        private readonly PostgresContext db;
+
+        public SensorDataController(ILogger<SensorDataController> logger, RabbitMQService rabbit, PostgresContext db)
         {
             _logger = logger;
             this.rabbit = rabbit;
+            this.db = db;
         }
 
         [HttpGet("{device}")]
@@ -32,6 +37,15 @@ namespace web.Controllers
             {
                 CancellationToken ct = HttpContext.RequestAborted;
                 using WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+                var recentData = db.SensorData.OrderByDescending(e => e.ReceivedAt).Take(100).ToList();
+                foreach(var row in recentData) 
+                {
+                    var dto = JsonSerializer.Serialize(new {temperature = row.Temperature, humidity = row.Humidity, ppm = row.Ppm, receivedAt = row.ReceivedAt});
+                    var body = Encoding.UTF8.GetBytes(dto);
+                    await webSocket.SendAsync(new ArraySegment<byte>(body, 0, body.Length), WebSocketMessageType.Text, true, ct);
+                }
+
                 var reader = rabbit.SubscribeAndWrap(device, ct);
 
                 try {
