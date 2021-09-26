99f0a5cacb1100cf0002a7f1f2b9863762ad6c50
Sergey Suglobov <gruzmob@gmail.com>
2021-09-27 01:22:17 +0900

    first attempt at sending data from zero

--- zero/src/app.py
+++ zero/src/app.py
@@ -1,23 +1,26 @@
+import pika, json, os
+
 import time
 import board
 import busio
 import adafruit_scd30
 
-# SCD-30 has tempremental I2C with clock stretching, datasheet recommends
-# starting at 50KHz
-i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
-scd = adafruit_scd30.SCD30(i2c)
+def main():
+    host = os.environ['RABBITMQ_HOST']
+    device_id = os.environ['DEVICE_ID']
 
-while True:
-    # since the measurement interval is long (2+ seconds) we check for new data before reading
-    # the values, to ensure current readings.
-    if scd.data_available:
-        print("Data Available!")
-        print("CO2: %d PPM" % scd.CO2)
-        print("Temperature: %0.2f degrees C" % scd.temperature)
-        print("Humidity: %0.2f %% rH" % scd.relative_humidity)
-        print("")
-        print("Waiting for new data...")
-        print("")
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
+    scd = adafruit_scd30.SCD30(i2c)
 
-    time.sleep(0.5)
+    with pika.BlockingConnection(pika.ConnectionParameters(host=host)) as connection:
+        while True:
+            if scd.data_available:
+                data = {"ppm":scd.CO2, "temperature":scd.temperature, "humidity":scd.relative_humidity}
+                channel = connection.channel()
+                channel.basic_publish('amq.topic',
+                      f"sensor/live/data/{device_id}",
+                      json.dumps(data))
+
+            time.sleep(0.5)
+
+main()
