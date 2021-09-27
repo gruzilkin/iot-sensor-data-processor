8656b938ff05a43fa1776ab3f344c749ae72ee43
Sergey Suglobov <gruzmob@gmail.com>
2021-09-27 16:51:43 +0900

    removes code that was needed for iskra board

--- db/1-tables.sql
+++ db/1-tables.sql
@@ -6,18 +6,3 @@ CREATE TABLE sensor_data (
     ppm numeric,
     received_at timestamp NOT NULL
 );
-
-CREATE TABLE sensor_calibration_data (
-    id SERIAL PRIMARY KEY,
-    device_id varchar(32) NOT NULL,
-    temperature numeric NOT NULL,
-    humidity numeric NOT NULL,
-    r0 numeric NOT NULL,
-    ppm numeric NOT NULL,
-    uptime interval NOT NULL,
-    received_at timestamp NOT NULL,
-    is_invalid boolean DEFAULT false,
-    is_outlier boolean DEFAULT false
-);
-
-CREATE TABLE calibration_models (device_id varchar(32) PRIMARY KEY, model bytea NOT NULL, created_at timestamp NOT NULL);
