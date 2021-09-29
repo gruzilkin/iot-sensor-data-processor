c5124e6b55d4f583e8ac49e2c2f2ed44519a9423
Sergey Suglobov <gruzmob@gmail.com>
2021-09-29 19:37:38 +0900

    cleans up code and removes unused features

--- db_writer/src/app.py
+++ db_writer/src/app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from retry import retry
 from functools import partial
 
-def rabbit_callback(ch, method, properties, body, connection, table_name, body_dict_keys, body_dict_parsers={}):
+def rabbit_callback(ch, method, properties, body, connection):
     print(f"Received {body}")
     timestamp = properties.headers['timestamp_in_ms']
     body_dict = json.loads(body)
@@ -11,14 +11,13 @@ def rabbit_callback(ch, method, properties, body, connection, table_name, body_d
     received_at = datetime.fromtimestamp(timestamp/1000)
     device_id = method.routing_key.split('.')[-1]
 
-    body_dict_keys = [key for key in body_dict_keys if key in body_dict.keys()]
-    data = [body_dict_parsers[key](body_dict[key]) if key in body_dict_parsers.keys() else body_dict[key] for key in body_dict_keys]
+    body_dict_keys=['temperature', 'humidity', 'ppm']
+    data = [body_dict[key] for key in body_dict_keys]
 
-    cur = connection.cursor()
-    sql_args = (device_id, *data, received_at)
-    cur.execute(f"INSERT INTO {table_name} (device_id, {','.join(body_dict_keys)}, received_at) VALUES ({','.join(['%s'] *len(sql_args))})", sql_args)
-    connection.commit()
-    cur.close()
+    with connection.cursor() as cur:
+        sql_args = (device_id, *data, received_at)
+        cur.execute(f"INSERT INTO sensor_data (device_id, {','.join(body_dict_keys)}, received_at) VALUES ({','.join(['%s'] * len(sql_args))})", sql_args)
+        connection.commit()
 
 def create_connection():
     dbname = os.environ['DB_NAME']
@@ -34,21 +33,13 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     print(f"Pika connected to {host}")
 
-    channel = connection.channel()
+    with connection.channel() as channel:
+        channel.queue_declare(queue='sensor.data', durable=True)
+        channel.queue_bind(routing_key="sensor.data.*", queue='sensor.data', exchange="amq.topic")
 
-    channel.queue_declare(queue='sensor.data', durable=True)
-    channel.queue_bind(routing_key="sensor.data.*", queue='sensor.data', exchange="amq.topic")
-
-    conn = create_connection()
-    sensor_callback = partial(rabbit_callback, connection=conn, table_name='sensor_data',
-        body_dict_keys=['temperature', 'humidity', 'ppm'])
-
-    channel.basic_consume(queue='sensor.data', on_message_callback=sensor_callback, auto_ack=True)
-
-    try:
-        channel.start_consuming()
-    finally:
-        channel.close()
-        conn.close()
+        with create_connection() as conn:
+            sensor_callback = partial(rabbit_callback, connection=conn)
+            channel.basic_consume(queue='sensor.data', on_message_callback=sensor_callback, auto_ack=True)
+            channel.start_consuming()
     
 main()
