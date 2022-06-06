0ec8b3491baafc2b76072ddb437893594a8f6753
Sergey Suglobov <gruzmob@gmail.com>
2022-06-07 08:12:16 +0900

    effective storage notebook implementation

--- db/1-tables.sql
+++ db/1-tables.sql
@@ -13,3 +13,23 @@ CREATE TABLE sensor_data_sgp40 (
     voc numeric NOT NULL,
     received_at timestamp with time zone NOT NULL
 );
+
+CREATE TABLE weights_scd30_ppm (
+    id integer NOT NULL REFERENCES sensor_data_scd30 ON DELETE CASCADE,
+    weight numeric NOT NULL
+);
+
+CREATE TABLE weights_scd30_temperature (
+    id integer NOT NULL REFERENCES sensor_data_scd30 ON DELETE CASCADE,
+    weight numeric NOT NULL
+);
+
+CREATE TABLE weights_scd30_humidity (
+    id integer NOT NULL REFERENCES sensor_data_scd30 ON DELETE CASCADE,
+    weight numeric NOT NULL
+);
+
+CREATE TABLE weights_sgp40_voc (
+    id integer NOT NULL REFERENCES sensor_data_sgp40 ON DELETE CASCADE,
+    weight numeric NOT NULL
+);
