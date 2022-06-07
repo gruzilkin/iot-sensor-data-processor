bb0959ed19e06b6d3f5e6e9b275255c560d0538b
Sergey Suglobov <gruzmob@gmail.com>
2022-06-08 01:56:28 +0900

    signifficantly improved algorithm it can process data incrementally in small chunks good for raspberry pi

--- db/1-tables.sql
+++ db/1-tables.sql
@@ -15,21 +15,21 @@ CREATE TABLE sensor_data_sgp40 (
 );
 
 CREATE TABLE weights_scd30_ppm (
-    id integer NOT NULL REFERENCES sensor_data_scd30 ON DELETE CASCADE,
+    id integer PRIMARY KEY REFERENCES sensor_data_scd30 ON DELETE CASCADE,
     weight numeric NOT NULL
 );
 
 CREATE TABLE weights_scd30_temperature (
-    id integer NOT NULL REFERENCES sensor_data_scd30 ON DELETE CASCADE,
+    id integer PRIMARY KEY REFERENCES sensor_data_scd30 ON DELETE CASCADE,
     weight numeric NOT NULL
 );
 
 CREATE TABLE weights_scd30_humidity (
-    id integer NOT NULL REFERENCES sensor_data_scd30 ON DELETE CASCADE,
+    id integer PRIMARY KEY REFERENCES sensor_data_scd30 ON DELETE CASCADE,
     weight numeric NOT NULL
 );
 
 CREATE TABLE weights_sgp40_voc (
-    id integer NOT NULL REFERENCES sensor_data_sgp40 ON DELETE CASCADE,
+    id integer PRIMARY KEY REFERENCES sensor_data_sgp40 ON DELETE CASCADE,
     weight numeric NOT NULL
 );
